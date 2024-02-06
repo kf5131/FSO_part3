0c13a4dbdbedc5829f18ffc705e6be833c8aab70
kf5131 <kf5131@icloud.com>
2024-02-06 15:39:50 +0100

    do Phonebook backend all steps

--- Phonebook_backend/index.js
+++ Phonebook_backend/index.js
@@ -1,7 +1,9 @@
+const express = require('express')
+const app = express()
 
+app.use(express.json())
 
-
-[
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -23,3 +25,51 @@
       "number": "39-23-6423122"
     }
 ]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+            <p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person){
+    res.json(person)
+  }else{
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+  if(!person.name || !person.number){
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if(persons.find(p => p.name === person.name)){
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  person.id = Math.floor(Math.random() * 10000)
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+const PORT = 3002 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
