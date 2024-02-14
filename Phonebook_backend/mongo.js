cb03dafecdbca1f806aff0974d4f1abce8a1ff21
kf5131 <kf5131@icloud.com>
2024-02-14 19:22:21 +0100

    do phonebook part 12

---
+++ Phonebook_backend/mongo.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.ytljt1d.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+
+
+try {
+    mongoose.connect(url);
+    console.log('Connected to MongoDB');
+} catch (error) {
+    console.error('Failed to connect to MongoDB:', error.message);
+    process.exit(1);
+}
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log('phonebook:');
+        result.forEach(person => {
+            console.log(person.name, person.number);
+        });
+        mongoose.connection.close();
+    });
+}
+
+if (process.argv.length === 5) {
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    const person = new Person({
+        name: name,
+        number: number
+    });
+
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.connection.close();
+    });
+}
+
+if (process.argv.length > 5) {
+    console.log('Please provide the password as an argument: node mongo.js <password>');
+    process.exit(1);
+}
+
+// const person = new Person({
+//     name: 'Arto Hellas',
+//     number: '040-123456'
+// });
+
+
+// person.save().then(result => {
+//     console.log('person saved!');
+//     mongoose.connection.close();
+// });
+
+
+// Person.find({}).then(result => {
+//     result.forEach(person => {
+//         console.log(person);
+//     });
